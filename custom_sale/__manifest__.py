15713aa9cbc8d7fac33e38a7480cd5efa434f42b
mulenga-nsembe-israel <mulengaisrael5@gmail.com>
2026-04-29 14:16:27 +0200

    init custom_sale module

---
+++ custom_sale/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Custom Sale',
+    'summary': 'Adds a private logistics tab on quotes',
+    'description': 'Allows recording private technical notes on quotes for logistics, not visible to the client.',
+    'version': '1.0.0',
+    'category': 'Sales',
+    'author': 'Nexyor',
+    'depends': ['sale'],
+    'data': ['views/sale_order_views.xml'],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
